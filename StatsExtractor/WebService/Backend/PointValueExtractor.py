fixing backend/frontend web service to show moving average filters

StatsExtractor/WebService/Backend/PointValueExtractor.py
@@ -6,11 +6,8 @@ from Libs.Utils import xyToColRow, scaleValue, getListOfFiles
 
 
 class PointValueExtractor():
-    def __init__(self, inImages, inVariable, pattern, createDate, xCoord, yCoord, epsg="EPSG:4326"):
-        self._images = inImages
-        self._variable = inVariable
-        self._productPattern = re.compile(pattern)
-        self._createDate = createDate
+    def __init__(self, data, xCoord, yCoord, epsg="EPSG:4326"):
+        self._data = data
         self._xCoord = xCoord
         self._yCoord = yCoord
         self._pointEPSG = epsg
@@ -23,19 +20,22 @@ class PointValueExtractor():
         return None
 
     def __getNETCDFSubdataset(self, img):
-        return """NETCDF:"{0}":{1}""".format(img, self._variable)
+        return """NETCDF:"{0}":{1}""".format(img, self._data[0])
 
     def _movingAverage(self, ret,windowSize = 4):
         if ret is None or ret["raw"] is None:
             return
 
         window = np.ones(int(windowSize)) / float(windowSize)
+        print(ret)
 
         rawData = [s[1] for s in ret["raw"]]
         ll = np.convolve(rawData, window, mode='valid')
         difs = rawData[windowSize - 1::] - ll
         mn = difs.mean()
         std = difs.std()
+        difs = np.round(difs, 3)
+        print(mn,std)
         ret["filtered"] = [ [x[0],y] for x,y in zip(ret["raw"][windowSize - 1::],ll)]
         for rawVal, dif in zip(ret["raw"][windowSize - 1::], difs):
             rawVal.append(int(dif < mn-3*std)*(-3)
@@ -45,15 +45,23 @@ class PointValueExtractor():
             + int(dif >= mn + 1*std and dif <mn + 2*std)*(1)
             + int(dif >= mn + 2*std and dif <mn + 3*std)*(2)
             + int(dif >= mn + 3 * std)*(3))
+            print(int(dif < mn-3*std)*(-3)
+            + int(dif >= mn - 3*std and dif <mn - 2*std)*(-2)
+            + int(dif >= mn - 2*std and dif <mn - 1*std)*(-1)
+            + int(dif >= mn - 1*std and dif <mn + 1*std)*(0)
+            + int(dif >= mn + 1*std and dif <mn + 2*std)*(1)
+            + int(dif >= mn + 2*std and dif <mn + 3*std)*(2)
+            + int(dif >= mn + 3 * std)*(3))
+        print(ret)
 
     def process(self):
         ret = {}
-        ret["raw"] = list(range(len(self._images)))
+        ret["raw"] = list(range(len(self._data[1])))
         ret["filtered"] = {}
         i = 0
         try:
             transformed = False
-            for img in self._images:
+            for img in self._data[2]:
                 if not os.path.isfile(img):
                     raise FileExistsError
 
@@ -79,12 +87,12 @@ class PointValueExtractor():
                 gt = inData.GetGeoTransform()
                 col,row = xyToColRow(self._xCoord, self._yCoord, gt)
                 value = inData.GetRasterBand(1).ReadAsArray(col, row, 1, 1)[0,0]
-                ret["raw"][i] = [self.__createDate(img), value]
+                ret["raw"][i] = [self._data[2][img], value]
                 if value == inData.GetRasterBand(1).GetNoDataValue():
                     ret["raw"][i][1] = None
                 #applying netCDF scaling
                 else:
-                    ret["raw"][i][1] =  np.round(scaleValue(inData.GetMetadata(), ret["raw"][i][1], self._variable), 4)
+                    ret["raw"][i][1] =  np.round(scaleValue(inData.GetMetadata(), ret["raw"][i][1], self._data[0]), 4)
                 i += 1
 
         except FileExistsError:
@@ -97,22 +105,17 @@ class PointValueExtractor():
             print("Unable to extract statistics. Exiting")
             ret = None
 
-
         self._movingAverage(ret)
         return ret
 
 def main():
-    inputImageDir = "/home/madagu/Projects/JRCStatsExtractor/ExperimentalData/Imagery/BioPar_NDVI300_V2_Global/"
-    inImages = getListOfFiles(inputImageDir)
-    inImages = [img for img in inImages if img.endswith(".nc")]
-    variable = "NDVI"
-    pattern = "c_gls_NDVI300_(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})_GLOBE_OLCI_V2.0.1.nc"
-    createDate = "{0}-{1}-{2}T{3}:{4}:00"
+    data = ('NDVI', ['/home/madagu/Projects/JRCStatsExtractor/ExperimentalData/Imagery/BioPar_NDVI300_V2_Global/2020/20201101/c_gls_NDVI300_202011010000_GLOBE_OLCI_V2.0.1/c_gls_NDVI300_202011010000_GLOBE_OLCI_V2.0.1.nc', '/home/madagu/Projects/JRCStatsExtractor/ExperimentalData/Imagery/BioPar_NDVI300_V2_Global/2020/20201111/c_gls_NDVI300_202011110000_GLOBE_OLCI_V2.0.1/c_gls_NDVI300_202011110000_GLOBE_OLCI_V2.0.1.nc', '/home/madagu/Projects/JRCStatsExtractor/ExperimentalData/Imagery/BioPar_NDVI300_V2_Global/2021/20210411/c_gls_NDVI300_202104110000_GLOBE_OLCI_V2.0.1/c_gls_NDVI300_202104110000_GLOBE_OLCI_V2.0.1.nc', '/home/madagu/Projects/JRCStatsExtractor/ExperimentalData/Imagery/BioPar_NDVI300_V2_Global/2021/20210421/c_gls_NDVI300_202104210000_GLOBE_OLCI_V2.0.1/c_gls_NDVI300_202104210000_GLOBE_OLCI_V2.0.1.nc', '/home/madagu/Projects/JRCStatsExtractor/ExperimentalData/Imagery/BioPar_NDVI300_V2_Global/2021/20210621/c_gls_NDVI300_202106210000_GLOBE_OLCI_V2.0.1/c_gls_NDVI300_202106210000_GLOBE_OLCI_V2.0.1.nc', '/home/madagu/Projects/JRCStatsExtractor/ExperimentalData/Imagery/BioPar_NDVI300_V2_Global/2021/20210701/c_gls_NDVI300_202107010000_GLOBE_OLCI_V2.0.1/c_gls_NDVI300_202107010000_GLOBE_OLCI_V2.0.1.nc'], {'/home/madagu/Projects/JRCStatsExtractor/ExperimentalData/Imagery/BioPar_NDVI300_V2_Global/2020/20201101/c_gls_NDVI300_202011010000_GLOBE_OLCI_V2.0.1/c_gls_NDVI300_202011010000_GLOBE_OLCI_V2.0.1.nc': '2020-11-01T00:00:00', '/home/madagu/Projects/JRCStatsExtractor/ExperimentalData/Imagery/BioPar_NDVI300_V2_Global/2020/20201111/c_gls_NDVI300_202011110000_GLOBE_OLCI_V2.0.1/c_gls_NDVI300_202011110000_GLOBE_OLCI_V2.0.1.nc': '2020-11-11T00:00:00', '/home/madagu/Projects/JRCStatsExtractor/ExperimentalData/Imagery/BioPar_NDVI300_V2_Global/2021/20210411/c_gls_NDVI300_202104110000_GLOBE_OLCI_V2.0.1/c_gls_NDVI300_202104110000_GLOBE_OLCI_V2.0.1.nc': '2021-04-11T00:00:00', '/home/madagu/Projects/JRCStatsExtractor/ExperimentalData/Imagery/BioPar_NDVI300_V2_Global/2021/20210421/c_gls_NDVI300_202104210000_GLOBE_OLCI_V2.0.1/c_gls_NDVI300_202104210000_GLOBE_OLCI_V2.0.1.nc': '2021-04-21T00:00:00', '/home/madagu/Projects/JRCStatsExtractor/ExperimentalData/Imagery/BioPar_NDVI300_V2_Global/2021/20210621/c_gls_NDVI300_202106210000_GLOBE_OLCI_V2.0.1/c_gls_NDVI300_202106210000_GLOBE_OLCI_V2.0.1.nc': '2021-06-21T00:00:00', '/home/madagu/Projects/JRCStatsExtractor/ExperimentalData/Imagery/BioPar_NDVI300_V2_Global/2021/20210701/c_gls_NDVI300_202107010000_GLOBE_OLCI_V2.0.1/c_gls_NDVI300_202107010000_GLOBE_OLCI_V2.0.1.nc': '2021-07-01T00:00:00'}, 'c_gls_NDVI300_(\\d{4})(\\d{2})(\\d{2})(\\d{2})(\\d{2})_GLOBE_OLCI_V2.0.1.nc', '(int, int, int, int, int,)', '{0}-{1}-{2}T{3}:{4}:00')
+
     xCoord = 3002465.761143498
     yCoord = 990286.9527982064
     epsg = "EPSG:3857"
 
-    obj = PointValueExtractor(inImages, variable, pattern, createDate, xCoord, yCoord, epsg)
+    obj = PointValueExtractor(data, xCoord, yCoord, epsg)
     val = obj.process()
 
 