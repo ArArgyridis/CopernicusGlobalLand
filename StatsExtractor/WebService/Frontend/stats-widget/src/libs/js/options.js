d0fe4c84c9f029431cbf4588a202b7fccfece90d
ARGYRIDIS Argyros <argyros.argyridis@ext.ec.europa.eu>
2023-02-13 13:22:19 +0200

    improved url detection

--- StatsExtractor/WebService/Frontend/stats-widget/src/libs/js/options.js
+++ StatsExtractor/WebService/Frontend/stats-widget/src/libs/js/options.js
@@ -1,16 +1,6 @@
-/*
 export default {
-	endpointURL: process.env.NODE_ENV == "development" ? "http://localhost/endpoint" : "http://185.213.74.224/endpoint",
-	wmsURL: process.env.NODE_ENV == "development" ? "http://localhost/wms/" : "http://185.213.74.224/wms",
-	anomaliesWMSURL: process.env.NODE_ENV == "development" ? "http://localhost/anomalieswms/" : "http://185.213.74.224/wms"
-} 
-*/
-
-
-export default {
-	endpointURL: "http://localhost/endpoint",
-	wmsURL: "http://localhost/wms/",
-	anomaliesWMSURL: "http://localhost/anomalieswms/",
+	endpointURL: "http://" + location.hostname + "/endpoint",
+	wmsURL: "http://" + location.hostname + "/wms/",
+	anomaliesWMSURL: "http://" + location.hostname + "/anomalieswms/",
 	bingKey: "Aiq5aNY4e4kv_7oWzzn9m2zdnfBjabNG8Lc39kAnE-SuAYaeDWruOiRrk1RhIGYl"
 }
-
