b505d949f0df3266e0a37594a1a49b9cc113070b
Argyros Argyridis <arargyridis@gmail.com>
2023-03-01 12:49:53 +0200

    adding fix for configuration file

--- StatsExtractor/WebService/Backend/runWebService.sh
+++ StatsExtractor/WebService/Backend/runWebService.sh
@@ -3,6 +3,7 @@
 if  ps -aux | grep -v grep |grep uwsgi; then
 	exit 0
 else
+	ln -s /Mapserver/active_config.json /usr/src/app/StatsExtractor
 	nohup uwsgi statsservice.ini > statsservice_nohup.out 2>&1 &
 	exit 0
 fi
