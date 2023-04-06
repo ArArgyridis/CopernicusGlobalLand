c13de22fac41828e10059247568fa7f1362953ae
Argyros Argyridis <arargyridis@gmail.com>
2023-04-06 16:59:35 +0300

    changing mapcache path

--- StatsExtractor/WebService/Backend/runWebService.sh
+++ StatsExtractor/WebService/Backend/runWebService.sh
@@ -5,7 +5,7 @@ if  ps -aux | grep -v grep |grep uwsgi; then
 else
 	ln -s /Mapserver/active_config.json /usr/src/app/StatsExtractor
 	nohup uwsgi statsservice.ini > statsservice_nohup.out 2>&1 &
-	export MAPCACHE_CONFIG_FILE=/etc/nginx/conf.d/stratifications.xml
+	export MAPCACHE_CONFIG_FILE=/Data/stratifications.xml
 	nohup spawn-fcgi -a 127.0.0.1 -p 1338 -F 6 -u $USER -U $USER /usr/bin/mapcache.fcgi >/dev/null 2>&1  &
 	exit 0
 fi
