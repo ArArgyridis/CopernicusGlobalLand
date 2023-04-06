f7fb02e5e7ea1708858db3a608f0282cf8eac679
Argyros Argyridis <arargyridis@gmail.com>
2023-04-06 16:40:50 +0300

    adding stopping of mapcache service

--- StatsExtractor/WebService/Backend/stopWebService.sh
+++ StatsExtractor/WebService/Backend/stopWebService.sh
@@ -4,3 +4,8 @@ if  ps -aux | grep -v grep |grep uwsgi; then
 	uwsgi --stop /tmp/stratification_stats.pid
 	exit 0
 fi
+
+if  ps -aux | grep -v grep |grep mapcache.fcgi; then
+	killall mapcache.fcgi
+	exit 0
+fi
