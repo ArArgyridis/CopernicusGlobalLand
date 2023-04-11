ae2335dc1ae4e5a9ee1a08905091ea5cb272bbf0
ARGYRIDIS Argyros <argyros.argyridis@ext.ec.europa.eu>
2023-04-11 13:38:06 +0300

    updating backend startup script

--- StatsExtractor/WebService/Backend/runWebService.sh
+++ StatsExtractor/WebService/Backend/runWebService.sh
@@ -5,8 +5,8 @@ if  ps -aux | grep -v grep |grep uwsgi; then
 else
 	ln -s /Mapserver/active_config.json /usr/src/app/StatsExtractor
 	nohup uwsgi statsservice.ini > statsservice_nohup.out 2>&1 &
-	nohup spawn-fcgi -a 127.0.0.1 -p 1337 -F 1 -u $USER -U $USER /usr/bin/mapserv >/dev/null 2>&1  &
+	nohup spawn-fcgi -a 127.0.0.1 -p 1337 -F 1  /usr/bin/mapserv >/dev/null 2>&1  &
 	export MAPCACHE_CONFIG_FILE=/Data/stratifications.xml
-	nohup spawn-fcgi -a 127.0.0.1 -p 1338 -F 6 -u $USER -U $USER /usr/bin/mapcache.fcgi >/dev/null 2>&1  &
+	nohup spawn-fcgi -a 127.0.0.1 -p 1338 -F 6  /usr/bin/mapcache.fcgi >/dev/null 2>&1  &
 	exit 0
 fi
