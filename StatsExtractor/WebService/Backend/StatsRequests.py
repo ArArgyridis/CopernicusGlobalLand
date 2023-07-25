ab200abe504898cd9a5830bc5dadcf70a621a4ed
Argyros Argyridis <arargyridis@gmail.com>
2023-07-25 12:08:04 +0300

    syntactic fix

--- StatsExtractor/WebService/Backend/StatsRequests.py
+++ StatsExtractor/WebService/Backend/StatsRequests.py
@@ -76,7 +76,7 @@ class StatsRequests(GenericRequest):
     def __fetchProductInfo(self):
         query = """
              WITH dt AS( 
-        	SELECT pfd.id,  p.name[1], p.description, product_file_description_id, pfd.CASE WHEN rt_flag_pattern IS NOT NULL THEN TRUE ELSE FALSE END has_rt,
+        	SELECT pfd.id,  p.name[1], p.description, product_file_description_id, CASE WHEN pfd.rt_flag_pattern IS NOT NULL THEN TRUE ELSE FALSE END has_rt,
         	ARRAY_TO_JSON(ARRAY_AGG(row_to_json(pfv.*)::jsonb || jsonb_build_object('anomaly_info', anomaly_info.anomaly_info) ORDER BY pfv.description)) variables
 	        FROM product p 
         	JOIN product_file_description pfd ON p.id = pfd.product_id AND p.id != 10
