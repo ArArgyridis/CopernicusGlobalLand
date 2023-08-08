8a11b207670bf2f33888b01d626cc5bb6959bdaf
ARGYRIDIS Argyros <argyros.argyridis@ext.ec.europa.eu>
2023-08-08 09:12:58 +0200

    activating statistics computation inside service main loop

--- main.py
+++ main.py
@@ -66,16 +66,20 @@ def main():
 				mapserver = None
 
 			#fetching stratifications and compute stats for each strata
-			"""
+
 			query = "select description from stratification s "
 			print("Extracting statistics")
 			
 			res = cfg.pgConnections[cfg.statsInfo.connectionId].fetchQueryResult(query)
 			if res != 1:
 				for row in res:
+					statsCmd = """StatsExtractor "{0}" "{1}" """.format(config, row[0])
+					os.system(statsCmd)
+
+					"""
 					obj = ZonalStatsExtractor(row[0], config)
 					obj.process(productIds=[ Constants.PRODUCT_INFO[pid].id for pid in Constants.PRODUCT_INFO])
-			"""
+					"""
 			print("process completed! Waiting....")
 			sleep(43200)
 			#sleep(1)
