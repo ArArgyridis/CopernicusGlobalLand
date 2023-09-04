308996f9714deb7f896b68f25acc2f8c44932597
ARGYRIDIS Argyros <argyros.argyridis@ext.ec.europa.eu>
2023-09-04 13:31:47 +0300

    minor fix

--- main.py
+++ main.py
@@ -50,13 +50,8 @@ def main():
 					os.makedirs(tmpDir, exist_ok=True)
 
 				obj = DataCrawler(cfg, Constants.PRODUCT_INFO[pid], False)
-				if Constants.PRODUCT_INFO[pid].productType == "lts":
-					obj.fetchOrValidateAgainstVITO(dir="/home/argyros/Desktop/data/BIOPAR/",
-												   storageDir=cfg.filesystem.ltsPath)
-					continue
-				else:
-					obj.importProductFromLocalStorage(inDir)
-					#obj.fetchProductFromVITO(dir="/home/argyros/Desktop/data/BIOPAR/", storageDir=cfg.filesystem.imageryPath)
+				obj.importProductFromLocalStorage(inDir)
+				#obj.fetchProductFromVITO(dir="/home/argyros/Desktop/data/BIOPAR/", storageDir=cfg.filesystem.imageryPath)
 
 				#compute anomalies
 				del obj
