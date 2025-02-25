723d06d7a8d088ba4846ed836686b23ad7ba287b
Argyros Argyridis <ararygiridis@gmail.com>
2025-02-25 13:08:30 +0200

    improving NatStat's backend execution scripts

--- StatsExtractor/WebService/Backend/stopWebService.sh
+++ StatsExtractor/WebService/Backend/stopWebService.sh
@@ -1,9 +1,18 @@
 #! /bin/sh
 
-if  ps -aux | grep -v grep |grep uwsgi; then
-	uwsgi --stop /tmp/stratification_stats.pid
+if  ps -aux | grep -v grep |grep 'uwsgi NatStats.ini'; then
+	uwsgi --stop /tmp/NatStatsUWSGI.pid
 fi
 
 if  ps -aux | grep -v grep |grep fcgiwrap; then
-	killall fcgiwrap
+	ps aux | grep '[f]cgiwrap' | awk '{print $2}' | xargs kill -SIGTERM
 fi
+
+if ps -aux | grep -v grep |grep mapserv; then
+	killall -s SIGKILL mapserv
+fi
+
+if ps -aux | grep -v grep |grep mapcache.fcgi; then
+	killall -s SIGKILL mapcache.fcgi
+fi
+
