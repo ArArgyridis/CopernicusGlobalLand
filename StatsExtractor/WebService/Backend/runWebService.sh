124711a2c13bf4e0c202ff80b01804cddb8b2ccb
Argyros Argyridis <ararygiridis@gmail.com>
2025-02-25 13:14:49 +0200

    adding missing files

--- StatsExtractor/WebService/Backend/runWebService.sh
+++ StatsExtractor/WebService/Backend/runWebService.sh
@@ -5,8 +5,8 @@ if  ps -aux | grep -v grep |grep 'uwsgi statsservice.ini'; then
 else
 	ln -s /mnt/Data/natstats/active_config.json ../../
 	export MAPCACHE_CONFIG_FILE=/mnt/Data/natstats/mapcache/stratifications.xml
-	nohup uwsgi NatStats.ini > NatStats_nohup.out 2>&1 &
 	nohup spawn-fcgi -a 127.0.0.1 -p 1337 -f "/usr/sbin/fcgiwrap -c 12" >/dev/null 2>&1  &
-	nohup spawn-fcgi -a 127.0.0.1 -p 1338 -f "/usr/bin/mapcache.fcgi" > /dev/null 2>&1 &
+	nohup uwsgi NatStats.ini > NatStats_nohup.out 2>&1 &
+	nohup spawn-fcgi -a 127.0.0.1 -p 1339 -f "/usr/bin/mapcache.fcgi" > /dev/null 2>&1 &
 	exit 0
 fi
