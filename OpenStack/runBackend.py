e79152605a3c479b9b6ab043a0cae6e784cfe6a8
Argyros Argyridis <ararygiridis@gmail.com>
2025-09-12 15:25:13 +0300

    adding script to run machines in openstack environment

---
+++ OpenStack/runBackend.py
@@ -0,0 +1,105 @@
+import json, openstack, os, paramiko, subprocess, sys, time
+
+class OpenStackSSH(object):
+    def __init__(self, name, sshOptions):
+        self._name = name
+        self._client = paramiko.SSHClient()
+        self._client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self._options = sshOptions
+
+    def connect(self):
+        connected = False
+
+        while not connected:
+            try:
+                self._client.connect(hostname=self._options["host"], port=self._options["port"], username=self._options["user"], pkey=paramiko.RSAKey.from_private_key_file(self._options["key"], self._options["passphrase"]))
+                connected = True
+                print("Connection established to: ", self._name)
+            except:
+                print("Waiting for SSH connection ({0})...".format(self._name))
+                time.sleep(5)
+
+    def execCommands(self, commands):
+        for cmd in commands:
+            channel = self._client.invoke_shell()
+            channel.send(cmd["exec"])
+
+            log = open(cmd["logFile"], "w")
+
+            while not channel.recv_ready():
+                channel.recv(1024)
+                time.sleep(0.1)
+
+            # clear out any initial text
+
+            stop = False
+            while not stop:
+                output = channel.recv(1024).decode("utf-8")
+                log.write(output)
+                log.flush()
+                print(output, end='')
+                #stop = channel.exit_status_ready()
+                stop = output.strip().endswith("$") or output.strip().endswith("#")
+                time.sleep(0.1)
+
+
+    def __del__(self):
+        self._client.close()
+
+
+def runInstance(instance, ssh):
+    conn = openstack.connect(cloud=None)
+    server = conn.compute.get_server(instance["id"])
+
+    #check if server should be unshelved
+    if server.status == "SHELVED_OFFLOADED":
+        conn.compute.unshelve_server(server)
+
+    #for some reason the server is already computing...
+    if server.status == "ACTIVE":
+        return 1
+
+    sshCn = OpenStackSSH(instance["name"], ssh)
+
+    unShelved = False
+    while not unShelved:
+        server = conn.compute.get_server(instance["id"])
+        unShelved = server.status == "ACTIVE"
+        print("{0} status: {1}".format(server.name, server.status))
+        if not unShelved:
+            time.sleep(10)
+
+    sshCn.connect()
+    sshCn.execCommands(instance["commands"])
+    sshCn = None
+
+    conn.compute.shelve_server(server)
+    while unShelved:
+        server = conn.compute.get_server(instance["id"])
+        unShelved = server.status != "SHELVED_OFFLOADED"
+        print("{0} status: {1}".format(server.name, server.status))
+        if unShelved:
+            time.sleep(10)
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python runBackend.py config.json")
+        return 1
+
+    configOptions=json.load(open(sys.argv[1]))
+
+    for key in configOptions["credentials"]:
+        os.environ[key] = configOptions["credentials"][key]
+
+    #running only for first instance, if needed this can be done in parallel for multiple ones!
+    runInstance(configOptions["options"]["instances"][0], configOptions["options"]["ssh"][configOptions["options"]["instances"][0]["ssh"]])
+
+
+
+    return 0
+
+
+
+if __name__ == "__main__":
+    main()
